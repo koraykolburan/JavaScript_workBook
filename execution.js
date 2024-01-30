57e8de1863ad05ad970bea49d2047cff07215feb
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-01-30 23:05:54 +0300

    JavaScript Data Types

---
+++ execution.js
@@ -0,0 +1,42 @@
+alert("Koray");
+
+var firstName;
+firstName = "Koray";
+
+// NUMBERS AND STRING ----------------------------------------
+
+var lastName;
+lastName = 'Kolburan';
+
+var age;
+age = 28;
+
+var nameAndSurname;
+nameAndSurname = 'Koray Kolburan "with" it\'s escape character';
+
+var backslash;
+backslash = 'This is a backslash = \\'
+
+// firstName = "This is a tabbed \t text";
+// firstName = "Multi string\nsecond line text.";
+
+age = 28 + 1;
+age++;
+
+fullName = "Koray " + "Kolburan";
+
+// BOOLEAN -------------------------------------------------------
+
+var newA = true && true; //true
+var newB = true && false; //false
+
+var newC = true || false; //true
+var newD = false || false; //false
+
+var newE = !true;
+var newF = !false;
+
+var isSunny = true;
+var isSunday = false;
+
+var isAGoodDay = isSunny && isSunday;
