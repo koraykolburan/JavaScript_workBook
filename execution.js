581351a7d5e1cee759ab6053a62f1b0f1d2d6f5e
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-01-31 23:05:13 +0300

    Functions and Variable Scope
    Update JavaScript Documentation

--- execution.js
+++ execution.js
@@ -40,3 +40,32 @@ var isSunny = true;
 var isSunday = false;
 
 var isAGoodDay = isSunny && isSunday;
+
+// FUNCTIONS -------------------------------
+
+function getPercentage(percent, amount) {
+    //
+
+    var result
+    result = amount * percent / 100;
+
+    return result;
+}
+// This will work the function getPercentage(){} and it will write on HTML.
+document.getElementById("demo").innerHTML = getPercentage(25,100);
+
+function multiplyNumbers(number1, number2){
+
+    var result
+    result = number1 * number2;
+
+    return result
+}
+document.getElementById("demo1").innerHTML = multiplyNumbers(2,100);
+
+// SCOPES OF VARIABLES
+var fName = 'Koray';
+
+function accessGlobalScope() {
+    fName = 'The name has been changed!';
+}
