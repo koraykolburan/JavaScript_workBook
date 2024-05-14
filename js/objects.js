673362dd572782f35a3e7cbcf71e4e28c87d0acf
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-05-14 19:37:48 +0300

    JS Object Advanced - 3

--- js/objects.js
+++ js/objects.js
@@ -150,10 +150,126 @@ demo5.innerHTML =
 "In general, The name constructor functions are upper-case first letter. <br>" +
 "<span>function <b>Bike</b>(brand, type, model, color) { <br>"+
 " this.bikeBrands = brand;<br>"+ 
+" this.bikeType = type;<br>"+ 
 "this.bikeModels = model;<br>"+ 
 "this.bikeColors = color; <br>" + 
 "}</span> <br>" +
+"<span># NOTE # Bike is <b>Object Constructor function.</b></span> <br>" +
 "const myBikes = new Bike( 'Cannondale', 'Road Bike', 2024, 'Carbon Grey' ); <br>" +
-"The output is: Cannondale, Road Bike, 2024, Carbon Grey. <b>This example created one single object.</b><br>" +
+"The output is: Cannondale, Road Bike, 2024, Carbon Grey. <b> <br>" +
+"This example created one single object.</b><br>" +
 "<p>In a constructor function <b>this</b> keyword is a <b>substitute</b> for the new object. <br>" + 
-" The value of <b>this</b> will become the new object when a new object is created.</p>";
+" The value of <b>this</b> will become the new object when a new object is created.</p>" +
+"We can add a new property to the Object that we've created before. <br>" +
+"However, we <b>cannot add</b> a new property to a constructor like this: <b>Bike.madeIn = 'Germany'</b> <br>" + 
+"If we want to add <b>a new Property</b> to Constructor Function, we must add it to the constructor function. <br>" +
+"We <b>cannot add</b> a <b>new Method</b> to an Object Constructor. It must be done <b>inside the constructor function</b> <br>" +
+"We can add a new Method to an Object.";
+
+function Bike(brand, type, model, color) {
+     this.bikeBrand = brand;
+     this.bikeType = type;
+     this.bikeModel = model;
+     this.bikeColor = color;
+     this.changeModel = function ( newModel ) {
+          this.bikeModel = newModel;
+     }
+} 
+
+const myFirstBike = new Bike ("Cannondale", "Road Bike", 2018, "Claret Red");
+const mySecondBike = new Bike ("Canyon", "Trail Bike", 2022, "Titan Grey");
+
+myFirstBike.madeIn = "Vietnam"; // Adding a new Property to an Object 
+mySecondBike.madeIn = "Germany"; 
+myFirstBike.changeModel(2017); //This is from this.changeModel = function (newmodel){this.bikeModel = newModel};
+
+myFirstBike.brandAndType = function () {      //Adding a new METHOD to an Object
+     return this.bikeBrand + ", " + this.bikeType + ".";
+};
+
+let demo6 = document.getElementById("demo6");
+demo6.innerHTML = 
+"<h3>Object Contructor Function Example:</h3>" +
+"My first professional bike's brand is " + myFirstBike.bikeBrand + ". " +
+"And type of this is " + myFirstBike.bikeType + ". " + " It's model is " + myFirstBike.bikeModel +
+", it made in " + myFirstBike.madeIn + ". " + "At that time, I really liked the " + myFirstBike.bikeColor + " one. <br>" + 
+"Also I have a second one to ride on mountains that is " + mySecondBike.bikeBrand + ". " +
+"It has these properties, " + mySecondBike.bikeType + ", " + mySecondBike.bikeModel + ", it made in " + mySecondBike.madeIn + ". And it's color is " + mySecondBike.bikeColor + ". <br>" +
+"This is for adding a new method: " + myFirstBike.brandAndType() + "<br>" +
+"We added a new Method to Object Constructor with a <b>changeModel</b> name and output is here: " + myFirstBike.bikeModel + "<hr>";
+
+let demo7 = document.getElementById("demo7");
+demo7.innerHTML = 
+" <b># NOTE # </b> If we use all of these with a <b>new</b> keyword, their types are gonna be <b>Object</b>, except function. <br>"+ "new String(); new Number(); new Boolean(); new Object(); new Array(); new RegExp(); new Function(); new Date();" + "That's why, there is no need to use new String(), new Number(), new Boolean(), new Array(), and new RegExp(). <br>" +
+"Instead of these with <b>new</b>, we can use <b>literals</b> like: myArray = []... <br>" +
+"Math() is a <b>global object</b> it cannot be used with <b>new</b> keyword. <br>" +
+"<b>Primitive Values</b> are <u>much faster</u> like: let x1 = 0; or let x2 = 'abc' or let x3 = false... <br> " +
+"<h2>Object Prototype Inheritance</h2>" +
+"All JavaScript objects inherit properties and methods from a <b>prototype.</b> <br>" +
+"For example: Date object inherit from <b>Date.prototype</b> <br>"+
+"Or Array object inherit from <b>Array.prototype</b> <br>" + 
+"Or Bike object inherit from <b>Bike.prototype</b> <br>" +
+"The <b>Object.prototype</b> is on the <b>top</b> of the prototype inheritance chain! <br>" +
+"We can <b>ADD new Properties or New Methods</b> to <b>OBJECT CONSTRUCTOR</b> with <b>prototype</b> like these: <br>" +
+"Bike.prototype.madeIn = 'Germany'; <b>or</b> <br>" +
+"Bike.prototype.twoProp = function() { <br>" + 
+"return this.bikeModel + ' ' + this.bikeColor; <br>" +
+" } <br>" +
+" <b># Warning #</b>  -- Only <b>modify</b> your own prototypes. Never modify the prototypes of standard JavaScript Objects!!!";
+
+Bike.prototype.tires = "700 x 25c";
+Bike.prototype.twoProp = function() {
+     return this.bikeModel + ", " + this.bikeColor;
+}
+
+document.getElementById("demo8").innerHTML = 
+"This <b>Method</b> and <b>Property</b> was added via <b>Object Prototype</b> to Object Constructor: <br>" +
+"The size of tires of my first bike is " + myFirstBike.tires + 
+", also my second bike has more two properties like these: " + mySecondBike.twoProp() + ".";
+
+let demo9 = document.getElementById("demo9");
+demo9.innerHTML =
+"<ol>" + 
+"<li>Object.defineProperty(object, property, descriptor)</li> Adding an object property <br>" +
+"<span><b>Add a Property</b> Object.defineProperty(personId3, 'language', {value:'English'})</span> <br>" +
+"<span><b>personId3.language </b> : English.</span> <br>" +
+"Changing an object property <br>"+
+"<span><b>Changing a Property</b> Object.defineProperty(personId3, 'lastName', {value:'Unknown'})</span> <br>" +
+"<span><b>personId3.lastName </b> : Unknown.</span> <br>" +
+"<li>Object.defineProperties(object, descriptors)</li> Adding or changing object properties <br>" +
+"<li>Object.getOwnPropertyDescriptor(object, property)</li> Accessing a Property <br>" +
+"<li>Object.getOwnPropertyDescriptors(object)</li> Accessing Properties <br>" +
+"<li>Object.getOwnPropertyNames(object)</li> Returns all properties as an <b>Array</b> <br>" +
+"With this, we can <b>list</b> object properties Object.getOwnPropertyNames()personId3; <br>" +
+"<li>Object.getPrototypeOf(object)</li> Accessing the Prototype <br>" +
+"</ol>" +
+"<h4>Also we can change the Meta Data doing like these:</h4> <br>" +
+"writeable: false // Property value cannot be change. <br>" +
+"enumerable: false // Property value cannot be enumerated. <br>" +
+"configurable: false // Property value cannot be reconfigured. <br>" +
+"<h4>Also Getters and Setters can be changed: </h4>" +
+"Defining a Getter: <b>get: function() { return language }</b> <br>" +
+"Defining a Setter: <b>set: function(value) { language = value }</b> <br>" +
+"We can make language read-only as like this: <b>Object.defineProperty(personId3, 'language', {writable: false}).</b><br>";
+
+const personId3 = {
+     firstName: "Tom",
+     lastName: "Tom2",
+     age: 30
+}
+Object.defineProperty(personId3, "language", {value:"English"}); //Adding property
+Object.defineProperty(personId3, "lastName", {value:"Unknown"}); //Changing property
+Object.defineProperty(personId3, "language", {enumerable:false}); //Enumerable: false this property
+Object.defineProperty(personId3, "fullName", {                   //Adding a getter
+     get: function () { return this.firstName + " " + this.lastName;}
+});
+
+document.getElementById("demo10").innerHTML = 
+"This <b>list</b> all the properties of an <b>personId3 Object:</b> " + Object.getOwnPropertyNames(personId3) + "<br>" +
+"In here, we wrote this: <b>Object.defineProperty(personId3, 'language', {enumerable:false});</b> <br>" +
+"The result of this: " + Object.keys(personId3) + "<br>" +
+"There is no <b>'language'</b> property, because 'language' property is <b>not</b> enumerable anymore! <br>" +
+"<b>The Object.getOwnPropertyNames()</b> method returns all properties. <br>" +
+"However, <b>The Object.keys()</b> method returns all enumerable properties. <br>" + 
+"Example of defining a Getter and a Setter using Object.defineProperty() <br>" +
+personId3.fullName;
