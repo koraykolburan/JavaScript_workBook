e6291c647d3c9b557983d864e91e35557076e670
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-05-15 18:04:03 +0300

    JS - Functions Advanced - 1

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+let demo12 = document.getElementById("demo12");
+demo12.innerHTML =
+"<h1>JS Functions</h1>" +
+"JS functions are callable objects. We can use a function declaration or a function expression. <br>" +
+"Declared functions are not executed immediately, they are <b>saved for later use</b>, and will be executed later, when they are invoked or called. <br>" +
+"A JS function can also be defined using an <b>expression</b> like this: <br>" +
+"A function expression can be stored in a variable: <br>" +
+"<b>const</b> x = <b>function</b> (a, b) { return a * b };"+
+"";
