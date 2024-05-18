2b0d3360e999bf1904341403b51476b2e4a1e796
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-05-18 20:33:30 +0300

    JS - Classes Advanced - 3

--- js/classes2.js
+++ js/classes2.js
@@ -65,6 +65,30 @@ const myBike2 = new bike98("Canyon", 2024);
 let demo21 = document.getElementById("demo21");
 demo21.innerHTML = 
 "This is a Class method named 'model', that returns the bike model: <br> " +
-"My first bike is " + myBike.brand + ". And it is " + myBike.model() + " years old." +
-"We can send parameters to Class methods: <br>" +
-"My new bike is " + myBike2.model(year) + " years old." + " Its brand " + myBike2.brand;
+"My first bike is " + myBike.brand + ". And it is " + myBike.model() + " years old. <br>" +
+" We can send <b>parameters</b> to Class methods like this: <br>" +
+"My new bike is " + myBike2.model(year) + " years old." + " Its brand " + myBike2.brand + "." +
+"<h3>To Create a Class Inheritance with the 'extends' keyword</h3>" +
+"class Book { <br>" + 
+" constructor(name) { <br>" +
+"    this.bookname = name; <br>" + 
+"    } <br>" + 
+"    current() { <br>" + 
+"    return 'I have a ' + this.bookname; <br>" + 
+"}} <br>" +
+"<br>" +
+"class Genre <b>extends</b> Book { <br>" +
+" constructor(name, genre) { <br>" +
+" super(brand); <br>" +
+" this.genre = genre; <br>" +
+"    } <br>" + 
+"    show() { <br>" + 
+"    return this.current() + ', it is a ' + this.genre; <br>" + 
+"    } }<br>" + 
+"<br>" +
+"let myBook = new Book('BookName1', 'Autobiography'); <br>" +
+"console.log(myBook.show();) <br>"+ 
+"The <b>super()</b> method refers to the parent class. <br>" +
+"By calling the <b>super()</b> method in the constructor method, we call <b>the parent's constructor method</b>" +
+" and gets access to <b>the parent's properties and methods.</b> <br>" +
+""
