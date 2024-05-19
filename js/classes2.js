57e52453f0996633438c614c1de11dfb8a67e9cf
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2024-05-19 11:25:23 +0300

    JS - Classes Advanced is done.

--- js/classes2.js
+++ js/classes2.js
@@ -79,7 +79,7 @@ demo21.innerHTML =
 "<br>" +
 "class Genre <b>extends</b> Book { <br>" +
 " constructor(name, genre) { <br>" +
-" super(brand); <br>" +
+" <b>super</b>(brand); <br>" +
 " this.genre = genre; <br>" +
 "    } <br>" + 
 "    show() { <br>" + 
@@ -90,5 +90,58 @@ demo21.innerHTML =
 "console.log(myBook.show();) <br>"+ 
 "The <b>super()</b> method refers to the parent class. <br>" +
 "By calling the <b>super()</b> method in the constructor method, we call <b>the parent's constructor method</b>" +
-" and gets access to <b>the parent's properties and methods.</b> <br>" +
-""
+" and gets Access to <b>the parent's properties and methods.</b> <br>" +
+"<h3>Getters & Setters</h3>" +
+"Classes allows us to use getters and setters <br>" +
+"If we want to do something special with the <b>value</b> before returning, we can use <b>getters and setters.</b> <br>" +
+"To add <b>getters and setters</b> in the class, we need to use <b>get and set</b> keyword like this: <br>" +
+"class Bike { constructor (brand){ this.bikename = brand; } <b>get</b> bName() { return this.bikename } } <br>" +
+"<b>set</b> bName(x) { this.bikename = x } } <br> " +
+"const myBike = new Bike('Canyon'); <br>" +
+"console.log(myBike.bName); <br>" +
+"<h4># NOTES #</h4>" +
+"<b>1)</b> Even if the <b>getter</b> method, we don't use the <b>parentheses</b> when we want to get the property value. <br>" +
+"<b>2)</b> Plus, we can use an underscore '_' character before the property name to separate the <b>getter/setter</b> from the <b>actual property.</b> <br>" +
+"<b>Example:</b> like this: <b>this._bikename = brand;</b> get bikename() {<b> return this._bikename</b>} set bikename(x) { this._bikename = x; } <br>" +
+"<b>3)</b> <b>On the contrary functions, and other JS declarations, Class declarations are not <u>hoisted.</u></b> <br>" +
+"Therefore, We must declare a Class before we can use it!(uttermost). <br>" +
+"The <b>default behaviour of JS</b> declarations are hoisting <b>(moving the declaration to the TOP).</b> <br>" +
+"<h3>Js Static Methods</h3>" +
+"We cannot <b>call a static method</b> on an <b>object</b>. Only on an <b>object class.</b> Like this:<br>" +
+"class Car { constructor(name) { this.name = name; } <b>static</b> hello() { return 'Static Method!'; } } <br>" +
+"const myCar = new Car('BMW'); <br>" +
+"We can call like this: console.log(Car.hello();) it returns: <b>Static Method!</b>. <br>" +
+"If we invoke like this: <b>myCar.hello();</b> it will throw an <b>Error.</b>";
+
+//Getters and Setters in Class Constructors
+class Bike97 {
+     constructor (brand) {
+          this.bikename = brand;
+     }
+
+     get bName() {
+          return this.bikename;
+     }
+
+     set bName(x) {
+          this.bikename = x;
+     }
+}
+
+const myBike3 = new Bike97("Canyon");
+console.log("This result is for getters and setters result: " + myBike3.bName);
+
+//Static method Example in Class
+class Car {
+     constructor (name) {
+          this.name = name;
+     }
+
+     static sMethod(x) {            
+          return "Hello from Static Method! to " + x.name;
+     }
+}
+const myCar = new Car ("BMW");
+console.log(Car.sMethod(myCar));
+//console.log(myCar.sMethod());  -- -- -- If we invoke like this, it Throw AN ERROR!
+//In order to use the 'myCar' object, we need to send it as parameter.
